3321283083f5ad756020135eb88ddea063ed6c56
ssankreacha <shailen198@gmail.com>
2024-09-24 10:33:52 +0100

    Initial commit

---
+++ automated amazon price tracker.py
@@ -0,0 +1,45 @@
+import requests
+import smtplib
+from headers import headers
+from bs4 import BeautifulSoup
+
+# TODO-TASK: Create automated Amazon price tracker; sending email when item price is less than/at current price.
+
+"""
+Task Checklist:
+
+1. Ask the user to input a URL of the item they want (completed)
+2. Extract the price of that item (completed)
+3. Establish connection with email client (completed)
+4. Utilise conditional statements (if else) for when price is less than or at a certain value (completed)
+"""
+
+# Mock website (not-live) used: https://appbrewery.github.io/instant_pot/
+
+# Ask user for URL (live website), Retrieve website code in text format and integrated headers
+URL = input("Please copy and paste the URL of the item you're searching for: ")
+response = requests.get(url=URL, headers=headers)
+price_info = response.text
+
+# Parse the HTML content, Retrieve Text and Remove Spaces
+soup = BeautifulSoup(price_info, "html.parser")
+pounds = soup.find_all(name="span", class_="a-price-whole")[0].get_text(strip=True)
+pence = soup.find_all(name="span", class_="a-price-fraction")[0].get_text(strip=True)
+current_price = float(f"{pounds}{pence}")
+target_price = 100.00
+
+# Send email to user, when price is below target,
+# Target price can be used as user's budget,
+if current_price < target_price:
+    sender_email = " "      # Enter your email
+    sender_password = " "   # Find your app password, this depends on your email domain
+    recipient_email = " "   # Enter recipient email
+
+    with smtplib.SMTP("smtp.gmail.com", port=587) as connection:
+        connection.starttls()
+        connection.login(user=sender_email, password=sender_password)
+        connection.sendmail(from_addr=sender_email,
+                            to_addrs=recipient_email,
+                            msg="Subject:Amazon Price Alert!\n\n"f"Hey, the price has lowered below your budget!"
+                                f"Click to purchase{URL}")
+        connection.close()
---
+++ headers.py
@@ -0,0 +1,17 @@
+# headers.py
+
+headers = {
+    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+    "Accept-Encoding": "gzip, deflate, br, zstd",
+    "Accept-Language": "en-GB,en-US;q=0.9,en;q=0.8",
+    "Priority": "u=0, i",
+    "Sec-Ch-Ua": "\"Chromium\";v=\"128\", \"Not;A=Brand\";v=\"24\", \"Google Chrome\";v=\"128\"",
+    "Sec-Ch-Ua-Mobile": "?0",
+    "Sec-Ch-Ua-Platform": "\"Windows\"",
+    "Sec-Fetch-Dest": "document",
+    "Sec-Fetch-Mode": "navigate",
+    "Sec-Fetch-Site": "cross-site",
+    "Sec-Fetch-User": "?1",
+    "Upgrade-Insecure-Requests": "1",
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/128.0.0.0 Safari/537.36",
+}
